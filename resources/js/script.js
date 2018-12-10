c18f4626cd9c4bba57bada5c22348460db191741
argenis0409 <argenis04_09@icloud.com>
2018-12-10 11:25:04 -0800

    connected all the nav elements and added scroll feature

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,14 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    $('.js--section-features').waypoint(function(direction) {
-    if (direction == "down") {
-    $('nav').addClass('sticky');
-    }else {
-    $('nav').removeClass('sticky');
-    }
+    // sticky nav start here
+    $('.js--section-features').waypoint(function (direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
 
     }, {
-     offset: '60px;'
+            offset: '60px;'
+        })
+
+
+    $('.js--scroll-to-plans').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-plans').offset().top }, 1000);
     })
- 
+
+    $('.js--scroll-to-start').click(function () {
+        $('html, body').animate({ scrollTop: $('.js--section-features').offset().top }, 1000);
+    })
+
+
+    $(function () {
+
+        $('a[href*="#"]').not('[href="#"]').click(function (event) {
+
+            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname
+
+            ) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+
+                if (target.length) {
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000);
+                    
+                }
+            }
+        });
+    });
+
 })
