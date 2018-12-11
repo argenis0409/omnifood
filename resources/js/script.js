9044d9922b5f45aaac9887adba58c49e340bd32b
argenis0409 <argenis04_09@icloud.com>
2018-12-11 13:58:31 -0800

    added animation to how it works, the cities and the plans sections

--- resources/js/script.js
+++ resources/js/script.js
@@ -36,7 +36,7 @@ $(document).ready(function () {
                     $('html, body').animate({
                         scrollTop: target.offset().top
                     }, 1000);
-                    
+
                 }
             }
         });
@@ -44,10 +44,28 @@ $(document).ready(function () {
 
     // animation scroll efect here
 
-    $('.js--wp-1').waypoint(function(direction) {
-    $('.js--wp-1').addClass('animated fadeIn')
+    $('.js--wp-1').waypoint(function (direction) {
+        $('.js--wp-1').addClass('animated fadeIn')
     }, {
-    offset: '50%'
-    })
+            offset: '50%'
+        })
+
+    $('.js--wp-2').waypoint(function (direction) {
+        $('.js--wp-2').addClass('animated fadeInUp')
+    }, {
+            offset: '50%'
+        })
+
+    $('.js--wp-3').waypoint(function (direction) {
+        $('.js--wp-3').addClass('animated fadeIn')
+    }, {
+            offset: '50%'
+        })
+
+    $('.js--wp-4').waypoint(function (direction) {
+        $('.js--wp-4').addClass('animated pulse')
+    }, {
+            offset: '50%'
+        })
 
 })
